b7914db6b46531a4e855ec44f72c132267977b2f
umeadipaul53 <umeadipaul53@gmail.com>
2026-02-08 21:40:51 +0100

    added backend url to axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://blog-backend-ewga.onrender.com"
+      ? "https://blog-backend-ewga.onrender.com/api"
       : "http://localhost:5000/api",
   withCredentials: true,
 });
